09565ad587fb4b01db7ee0505c59e0711a358cc0
jibon49 <imoinul749@gmail.com>
2023-11-06 23:56:46 +0600

    connected to mongodb

--- index.js
+++ index.js
@@ -31,14 +31,28 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const assignmentCollection = client.db('assignmentDB').collection('allAssignment');
+
+    app.post('/create-assignment', async(req,res)=>{
+        const createAssignment = req.body;
+        console.log(createAssignment);
+        const result = await assignmentCollection.insertOne(createAssignment);
+        res.send(result);
+    })
+
+    app.get('/assignments',async(req,res)=>{
+        const cursor = assignmentCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
